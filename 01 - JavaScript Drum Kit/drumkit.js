3f64dab69bf1722640937ced337605f50f138240
turuto <turuto@gmail.com>
2021-02-19 06:54:56 +0100

    drumkit finished

--- 01 - JavaScript Drum Kit/drumkit.js
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -1,5 +1,21 @@
 document.onkeypress = function (e) {
     e = e || window.event;
     // use e.keyCode
-    console.log(e);
+    const inputCode = e.key.toUpperCase().charCodeAt(0);
+    //BUSCAR EL .KEY CON ESE data-key
+    const elements = document.querySelectorAll(`[data-key="${inputCode}"]`);
+    playSound(elements);
 };
+
+function playSound([div, sound]) {
+    sound.currentTime = 0;
+    div.classList.add('playing');
+    setTimeout(function () {
+        div.classList.remove('playing');
+    }, 200);
+    sound.play();
+    sound.onended = function () {
+        console.log(div, 'ended');
+        div.classList.remove('playing');
+    }
+}
