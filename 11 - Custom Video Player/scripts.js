6b872b11542f3a80002d01361de0797e3d6ca594
Alberto García Ariza <turuto@users.noreply.github.com>
2021-03-16 10:39:26 +0100

    video controls finished

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,13 +1,15 @@
 //ELEMENTS
 const player = document.querySelector('.player');
 const movie = document.querySelector('video');
-const progress = document.querySelector('.progress');
-const progress_filled = document.querySelector('.progress__filled');
+const progressBar = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
 const buttonPlay = document.querySelector('.toggle');
 const rangeVol = document.querySelector('input[name="volume"]');
 const rangeSpeed = document.querySelector('input[name="playbackRate"]');
 const buttonSkipBack = document.querySelector('[data-skip="-10"]');
 const buttonSkipFwd = document.querySelector('[data-skip="25"]');
+let updateInterval;
+
 // INNER
 let video = {
     isPlaying: false
@@ -20,24 +22,47 @@ function togglePlay() {
     if (video.isPlaying) {
         btn.textContent = '❚ ❚';
         movie.play();
+        updateInterval = window.setInterval(updateProgress, 500);
+
     } else {
         btn.textContent = '►';
         movie.pause();
+        clearInterval(updateInterval);
     }
 }
 // SKIP & REWIND
 function skip() {
-    let goTo = movie.currentTime + parseInt(this.dataset.skip);
-    if (goTo < 0) { goTo = 0 };
-    if (goTo > movie.duration) { goTo = movie.duration };
-    movie.currentTime = goTo;
+    let goToTime = movie.currentTime + parseInt(this.dataset.skip);
+    if (goToTime < 0) { goToTime = 0 };
+    if (goToTime > movie.duration) { goToTime = movie.duration };
+    movie.currentTime = goToTime;
 }
 // VOLUME
+function changeVolume() {
+    movie.volume = this.value;
+}
 // SPEED
+function changeSpeed() {
+    movie.playbackRate = this.value;
+}
 //PROGRESS BAR
+function updateProgress() {
+    let percentagePlayed = ((movie.currentTime / movie.duration) * 100).toFixed(3);
+    progressFilled.style.flexBasis = percentagePlayed + '%';
+}
+
+function updateMoveTime(e) {
+    let perc = e.offsetX / this.clientWidth;
+    movie.currentTime = movie.duration * perc;
+    updateProgress();
+}
 
 // EVENTS
 buttonPlay.addEventListener('click', togglePlay);
 buttonSkipBack.addEventListener('click', skip);
 buttonSkipFwd.addEventListener('click', skip);
+rangeVol.addEventListener('mousemove', changeVolume);
+rangeSpeed.addEventListener('mousemove', changeSpeed);
+progressBar.addEventListener('click', updateMoveTime);
+
 
